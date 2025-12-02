14609f45aa88a6d7e3b8e2825f39a9215c290a22
Narender <ch.narender.reddi@gmail.com>
2025-12-02 22:19:42 +0530

    Update vpnproxy.js

--- www/vpnproxy.js
+++ www/vpnproxy.js
@@ -1,1 +1,25 @@
-module.exports={};
+var exec = require('cordova/exec');
+
+var PLUGIN_NAME = 'VpnProxyDetect';
+
+module.exports = {
+isVpnConnected: function () {
+return new Promise(function (resolve, reject) {
+exec(resolve, reject, PLUGIN_NAME, 'isVpnConnected', []);
+});
+},
+
+
+isProxyEnabled: function () {
+return new Promise(function (resolve, reject) {
+exec(resolve, reject, PLUGIN_NAME, 'isProxyEnabled', []);
+});
+},
+
+
+getProxyInfo: function () {
+return new Promise(function (resolve, reject) {
+exec(resolve, reject, PLUGIN_NAME, 'getProxyInfo', []);
+});
+}
+};
