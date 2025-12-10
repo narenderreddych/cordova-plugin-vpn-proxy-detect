1143402b56930378d6c10589b309c046ebe5830e
Narender <ch.narender.reddi@gmail.com>
2025-12-10 17:53:37 +0530

    Refactor VPN and proxy detection methods
    Refactor VPN and proxy detection to run in background for improved performance and responsiveness. Added additional checks for VPN interfaces and refined proxy detection logic.

--- src/ios/VpnProxyDetect.m
+++ src/ios/VpnProxyDetect.m
@@ -3,6 +3,8 @@
 #import <arpa/inet.h>
 #import <net/if.h>
 #import <UIKit/UIKit.h>
+#import <SystemConfiguration/SystemConfiguration.h>
+#import <CFNetwork/CFProxySupport.h>
 
 @interface VpnProxyDetect : CDVPlugin
 - (void)check:(CDVInvokedUrlCommand*)command;
@@ -11,50 +13,53 @@
 @implementation VpnProxyDetect
 
 - (void)check:(CDVInvokedUrlCommand*)command {
-
-    CDVPluginResult *pluginResult = nil;
-
-    @try {
-        BOOL vpn = [self isVpnActive];
-        BOOL proxy = [self isProxyEnabled];
-        BOOL mitm = [self isMitmPresent];
-
-        NSArray *ifaces = [self getInterfaceNames];
-        NSString *ip = [self getLocalIp];
-
-        NSDictionary *res = @{
-            @"vpnEnabled": @(vpn),
-            @"proxyEnabled": @(proxy),
-            @"mitmDetected": @(mitm),
-            @"interfaces": ifaces ?: @[],
-            @"ip": ip ?: [NSNull null]
-        };
-
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:res];
-    }
-    @catch (NSException *ex) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:ex.reason];
-    }
-
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    [self.commandDelegate runInBackground:^{
+        CDVPluginResult *pluginResult = nil;
+        
+        @try {
+            BOOL vpn = [self isVpnActive];
+            BOOL proxy = [self isProxyEnabled];
+            BOOL mitm = [self isMitmPresent];
+            NSArray *ifaces = [self getInterfaceNames];
+            NSString *ip = [self getLocalIp];
+            
+            NSDictionary *res = @{
+                @"vpnEnabled": @(vpn),
+                @"proxyEnabled": @(proxy),
+                @"mitmDetected": @(mitm),
+                @"interfaces": ifaces ?: @[],
+                @"ip": ip ?: [NSNull null]
+            };
+            
+            NSLog(@"[VpnProxyDetect] VPN:%d Proxy:%d IP:%@", vpn, proxy, ip ?: @"null");
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:res];
+        }
+        @catch (NSException *ex) {
+            NSLog(@"[VpnProxyDetect] Error: %@", ex.reason);
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:ex.reason];
+        }
+        
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
 }
-
-#pragma mark - VPN Detection
 
 - (BOOL)isVpnActive {
     struct ifaddrs *addrs;
     BOOL found = NO;
-
+    
     if (getifaddrs(&addrs) == 0) {
         struct ifaddrs *cursor = addrs;
         while (cursor) {
-            NSString *name = [NSString stringWithUTF8String:cursor->ifa_name].lowercaseString;
-            if ([name hasPrefix:@"utun"] ||
-                [name hasPrefix:@"ipsec"] ||
-                [name hasPrefix:@"ppp"] ||
-                [name hasPrefix:@"tap"]) {
-                found = YES;
-                break;
+            if (cursor->ifa_name) {
+                NSString *name = [NSString stringWithUTF8String:cursor->ifa_name].lowercaseString;
+                
+                if ([name hasPrefix:@"utun"] || [name hasPrefix:@"ipsec"] || 
+                    [name hasPrefix:@"ppp"] || [name hasPrefix:@"tap"] ||
+                    [name hasPrefix:@"tun"] || [name hasPrefix:@"wg"]) {
+                    NSLog(@"[VpnProxyDetect] VPN interface: %@", name);
+                    found = YES;
+                    break;
+                }
             }
             cursor = cursor->ifa_next;
         }
@@ -63,16 +68,16 @@
     return found;
 }
 
-#pragma mark - Interface List
-
 - (NSArray*)getInterfaceNames {
     NSMutableArray *arr = [NSMutableArray array];
     struct ifaddrs *addrs;
-
+    
     if (getifaddrs(&addrs) == 0) {
         struct ifaddrs *cursor = addrs;
         while (cursor) {
-            [arr addObject:[NSString stringWithUTF8String:cursor->ifa_name]];
+            if (cursor->ifa_name) {
+                [arr addObject:[NSString stringWithUTF8String:cursor->ifa_name]];
+            }
             cursor = cursor->ifa_next;
         }
         freeifaddrs(addrs);
@@ -80,51 +85,75 @@
     return arr;
 }
 
-#pragma mark - Local IP Address
-
 - (NSString*)getLocalIp {
     struct ifaddrs *addrs;
-
+    NSString *wifiAddress = nil;
+    NSString *cellularAddress = nil;
+    
     if (getifaddrs(&addrs) == 0) {
         struct ifaddrs *cursor = addrs;
         while (cursor) {
-            if (cursor->ifa_addr->sa_family == AF_INET) {
+            if (cursor->ifa_addr && cursor->ifa_addr->sa_family == AF_INET) {
                 char addrBuf[INET_ADDRSTRLEN];
                 struct sockaddr_in *sa = (struct sockaddr_in*)cursor->ifa_addr;
                 inet_ntop(AF_INET, &(sa->sin_addr), addrBuf, INET_ADDRSTRLEN);
-
+                
                 NSString *name = [NSString stringWithUTF8String:cursor->ifa_name];
-                if (![name hasPrefix:@"lo"]) {
-                    freeifaddrs(addrs);
-                    return [NSString stringWithUTF8String:addrBuf];
+                NSString *address = [NSString stringWithUTF8String:addrBuf];
+                
+                if ([name hasPrefix:@"en"]) {
+                    wifiAddress = address;
+                } else if ([name hasPrefix:@"pdp_ip"]) {
+                    cellularAddress = address;
+                } else if (![name hasPrefix:@"lo"] && !wifiAddress && !cellularAddress) {
+                    wifiAddress = address;
                 }
             }
             cursor = cursor->ifa_next;
         }
         freeifaddrs(addrs);
     }
-    return nil;
+    
+    return wifiAddress ?: cellularAddress;
 }
-
-#pragma mark - Proxy Detection
 
 - (BOOL)isProxyEnabled {
     NSDictionary *proxySettings = (__bridge_transfer NSDictionary *)CFNetworkCopySystemProxySettings();
-
-    NSString *host = proxySettings[(NSString *)kCFNetworkProxiesHTTPProxy];
-    NSNumber *port = proxySettings[(NSString *)kCFNetworkProxiesHTTPPort];
-
-    if (host && port && host.length > 0) {
+    
+    if (!proxySettings) return NO;
+    
+    NSString *httpHost = proxySettings[(NSString *)kCFNetworkProxiesHTTPProxy];
+    NSString *httpsHost = proxySettings[(NSString *)kCFNetworkProxiesHTTPSProxy];
+    
+    if ((httpHost && [httpHost isKindOfClass:[NSString class]] && httpHost.length > 0) ||
+        (httpsHost && [httpsHost isKindOfClass:[NSString class]] && httpsHost.length > 0)) {
+        NSLog(@"[VpnProxyDetect] Proxy detected: HTTP=%@, HTTPS=%@", httpHost, httpsHost);
         return YES;
     }
+    
     return NO;
 }
 
-#pragma mark - MITM Detection (Basic)
-
 - (BOOL)isMitmPresent {
-    // iOS does not allow detecting custom installed CAs reliably.
-    // Keeping conservative false.
+    @try {
+        NSDictionary *proxySettings = (__bridge_transfer NSDictionary *)CFNetworkCopySystemProxySettings();
+        
+        if (proxySettings) {
+            NSString *httpHost = proxySettings[(NSString *)kCFNetworkProxiesHTTPProxy];
+            NSNumber *httpPort = proxySettings[(NSString *)kCFNetworkProxiesHTTPPort];
+            
+            if (httpHost && [httpHost isEqualToString:@"127.0.0.1"]) {
+                if (httpPort && ([httpPort intValue] == 8888 || [httpPort intValue] == 8889)) {
+                    NSLog(@"[VpnProxyDetect] MITM detected: local proxy on port %@", httpPort);
+                    return YES;
+                }
+            }
+        }
+    }
+    @catch (NSException *exception) {
+        NSLog(@"[VpnProxyDetect] MITM detection error: %@", exception);
+    }
+    
     return NO;
 }
 
